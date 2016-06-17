8cfba78ab12db615d33dcd5cd96a9609eea3078d
luwen-huang <luwenh@mit.edu>
2016-06-17 10:14:45 -0600

    patch

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ module.exports = function(credentials) {
   return {
     handcarFetch: require('./fetch/handcarFetch')(credentials),
     qbankFetch: require('./fetch/qbankFetch')(credentials),
-    CheckMissionStatus: require('./dateUtil/CheckMissionStatus');
-    ConvertDateToDictionary: require('./dateUtil/ConvertDateToDictionary');
-    QBankSignature: require('./signingUtil/QBankSignature');
+    CheckMissionStatus: require('./dateUtil/CheckMissionStatus'),
+    ConvertDateToDictionary: require('./dateUtil/ConvertDateToDictionary'),
+    QBankSignature: require('./signingUtil/QBankSignature')
   }
 
 }
