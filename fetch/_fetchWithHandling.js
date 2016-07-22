48a42bbd37f91e8c8a26494131a1f9a4bb4e2284
luwen-huang <luwenh@mit.edu>
2016-07-23 09:26:23 +1200

    modified fetch's error output

--- fetch/_fetchWithHandling.js
+++ fetch/_fetchWithHandling.js
@@ -9,8 +9,7 @@ function _fetchWithHandling(url, fetchInit, successCallback, errorCallback) {
 
       } else {
           response.text().then(function (responseText) {
-              console.log('Not a 200 response: ' + url);
-              console.log('Error returned: ' + responseText);
+              console.error('Not a 200 response:', url, 'Error returned:', responseText);
             try {
               errorCallback(response);
             } catch (e) {
@@ -20,8 +19,7 @@ function _fetchWithHandling(url, fetchInit, successCallback, errorCallback) {
       }
   })
   .catch(function (error) {
-      console.log('Error fetching: ' + url);
-      console.log('Error with fetch! ' + error.message);
+      console.error('Error with fetch. url', url, error.message);
   });
 }
 
